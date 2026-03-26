08eda3d0b6267db31ed92b9d3010939c036ec710
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-26 21:11:33 +0530

    sqrpattern

--- sqaurepattern.py
+++ sqaurepattern.py
@@ -1,5 +1,6 @@
 n=int(input())
-for i in range(0,n):
-    for j in range(0,n):
-        print("*")
+for r in range(0,n):#step 1 :no.of rows we want.
+    for c in range(0,n):#no.of columns we want.
+#it has to be inside the inner loop because whenever the r=1 started it enters into the c loop then it prints or execute the whatever it having inside of it no.of times as we mentioned. 
+        print("*",end=" ")    
     print()
