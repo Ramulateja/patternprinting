06ce106d858dec2c44b17d70ed6e37eb66482a64
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-27 17:09:35 +0530

    reverserightangle

---
+++ reverse-right-angle.py
@@ -0,0 +1,7 @@
+n=4
+for r in range(0,n):
+#i am given the start from ex:4 as n and end is row value then step -1.
+#if row start from o then r=0 stop value means untill 1 it prints by removing 1
+    for c in range(n,r,-1):
+        print('*',end=" ")
+    print()
