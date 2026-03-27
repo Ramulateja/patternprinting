ab5aeaceb78f19ff443a04acb05e59653f8ad02b
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-27 15:17:11 +0530

    rightangle

--- right-angletri.py
+++ right-angletri.py
@@ -1,5 +1,5 @@
 n=int(input())
-for r in range(0,n):
-    for c in range(0,r+1):
-        print("*",end="")
+for r in range(0,n):#from 0 to n-1 not n because it doesn't take untill n.
+    for c in range(0,r+1):#from 0 to r+1 means lets consider the n=4,it print 0 to r+1"value is 1 because row=1". 
+        print("*",end=" ")
     print()
