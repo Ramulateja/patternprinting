51b50be00f2b37d7286a52bdbc396da199e606ac
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-28 22:57:01 +0530

    numberhalfpiramid

--- numbershalfpiramid.py
+++ numbershalfpiramid.py
@@ -1,5 +1,5 @@
 n=int(input())
 for r in range(1,n+1):
     for c in range(1,r+1):
-        print(c,end=" ")
+        print(c,end=" ")# we using c to print the values c is the value that prints from range.
     print()
