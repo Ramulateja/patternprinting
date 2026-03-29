115d22e515fdef89b0430d128ce6662b0c5ebc7f
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-29 19:42:54 +0530

    lefthalfpiramidcomm

--- lefthalfpiramid.py
+++ lefthalfpiramid.py
@@ -3,8 +3,12 @@ n=int(input())
 for r in range(1,n+1):#The loop iterate from 1 to n.
 #in this we have to print the stars at the end.
 #first we print specific no.of spaces based on the row value.
-#
-    for c in range(1,n-r+1):#in this loop the range is 1 to  
+#steps:
+#step 1: 1st line we have print n-row spaces means if n=4 and r=1 and +1 because end value it doesn't take y we use +1 we need to give 3 spaces.  
+#step 1: 1st line we have print n-row spaces means if n=4 and r=2 and +1 because end value it doesn't take y we use +1 we need to give 2 spaces.  
+#step 1: 1st line we have print n-row spaces means if n=4 and r=3 and +1 because end value it doesn't take y we use +1 we need to give 1 spaces.  
+#step 1: 1st line we have print n-row spaces means if n=4 and r=4 and +1 because end value it doesn't take y we use +1 we need to give 0 spaces.  
+    for c in range(1,n-r+1):#in this loop the range is 1 to ex:n=4 ,r=1 then 4-1 = 3 spaces.  
         print(" ",end=" ")
     for c in range(r):
         print("*",end=" ")
