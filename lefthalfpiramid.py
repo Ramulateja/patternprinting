7003ed451e636c8eca749591010570a33208b9f3
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-29 19:50:58 +0530

    lefthalfpiramidcomm

--- lefthalfpiramid.py
+++ lefthalfpiramid.py
@@ -4,12 +4,12 @@ for r in range(1,n+1):#The loop iterate from 1 to n.
 #in this we have to print the stars at the end.
 #first we print specific no.of spaces based on the row value.
 #steps:
-#step 1: 1st line we have print n-row spaces means if n=4 and r=1 and +1 because end value it doesn't take y we use +1 we need to give 3 spaces.  
-#step 1: 1st line we have print n-row spaces means if n=4 and r=2 and +1 because end value it doesn't take y we use +1 we need to give 2 spaces.  
-#step 1: 1st line we have print n-row spaces means if n=4 and r=3 and +1 because end value it doesn't take y we use +1 we need to give 1 spaces.  
-#step 1: 1st line we have print n-row spaces means if n=4 and r=4 and +1 because end value it doesn't take y we use +1 we need to give 0 spaces.  
+#step 1: 1st line we print n-row spaces means if n=4 and r=1 and +1 because end value it doesn't take y we use +1 we need to give 3 spaces.  
+#step 2: 1st line we print n-row spaces means if n=4 and r=2 and +1 because end value it doesn't take y we use +1 we need to give 2 spaces.  
+#step 3: 1st line we print n-row spaces means if n=4 and r=3 and +1 because end value it doesn't take y we use +1 we need to give 1 spaces.  
+#step 4: 1st line we print n-row spaces means if n=4 and r=4 and +1 because end value it doesn't take y we use +1 we need to give 0 spaces.  
     for c in range(1,n-r+1):#in this loop the range is 1 to ex:n=4 ,r=1 then 4-1 = 3 spaces.  
         print(" ",end=" ")
-    for c in range(r):
+    for c in range(1,r+1):
         print("*",end=" ")
     print()
