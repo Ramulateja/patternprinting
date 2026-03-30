680b39e9a1db159f3ac6626cbceb0930e9ed2a20
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-30 15:31:57 +0530

    refactor:spacing

--- full-piramid.py
+++ full-piramid.py
@@ -1,7 +1,7 @@
 n=int(input())
 for r in range(1,n+1):
-    for c in range(1,n-r+1):
-        print(" ",end=" ")
+    for c in range(1,n-r+1):#the star will print left by default the then we use spaces to push to the right side.
+        print("|",end=" ")#"|"consider it as a space to get idea how spacing loop works.
     for c in range(1,2*r):
         print("*",end=" ")
     print()
