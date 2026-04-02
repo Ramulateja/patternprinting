91761310948af5a4f78bd43d3a82662ed77015ad
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-04-02 21:29:25 +0530

    comment

--- diamond.py
+++ diamond.py
@@ -5,7 +5,7 @@ for r in range(1,n+1):
     for c in range(1,2*r):
         print("*",end=" ")
     print()
-for r in range(2,n+1):
+for r in range(2,n+1):#we start it from 2 because the star middle part printed twise with 9 stars so to remove that i was started it from 2 to n rows.
     for c in range(r-1):
         print(" ",end=" ")
     for c in range(2*(n-r)+1):
