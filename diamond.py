d7c88babce8a616f30f29838c43dc466f68cf002
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-04-02 21:07:37 +0530

    updt:codeimprovement

--- diamond.py
+++ diamond.py
@@ -5,7 +5,7 @@ for r in range(1,n+1):
     for c in range(1,2*r):
         print("*",end=" ")
     print()
-for r in range(1,n+1):
+for r in range(2,n+1):
     for c in range(r-1):
         print(" ",end=" ")
     for c in range(2*(n-r)+1):
